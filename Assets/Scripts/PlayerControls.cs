f8b008137408c3bb8babb528efbb99638dc6657e
Jakub <49901045+outerspace303@users.noreply.github.com>
2021-03-30 22:25:38 +0200

    removed new input system (eh), reorganized assets, added basic input management

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -2,30 +2,39 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class PlayerControls : MonoBehaviour
 {
-    [SerializeField] InputAction movement;
-
-    void Start()
-    {
-        
-    }
-
-    void OnEnable()
-    {
-        movement.Enable();
-    }
-
-    void OnDisable()
-    {
-        movement.Disable();
-    }
-
+    [SerializeField] private float controlSpeed = 10f;
+    [SerializeField] private float xRange = 5f;
+    [SerializeField] private float zRange = 5f;
+    
+    // Because of Blender: Z - right, left; Y - up, down; X - forward
     void Update()
     {
-        float horizontalThrow = movement.ReadValue<Vector2>().x;
-        float verticalThrow = movement.ReadValue<Vector2>().y;
+        ProcessTranslation();
+        ProcessRotation();
+    }
+
+    private void ProcessRotation()
+    {
+        //transform.localRotation = Quaternion.Euler();
+    }
+
+    void ProcessTranslation()
+    {
+        float xThrow = Input.GetAxis("Vertical");
+        float xOffset = xThrow * Time.deltaTime * controlSpeed;
+        float rawXPos = transform.localPosition.x + xOffset;
+        float clampedXPos = Mathf.Clamp(rawXPos, -xRange, xRange);
+
+        float zThrow = Input.GetAxis("Horizontal");
+        float zOffset = zThrow * Time.deltaTime * controlSpeed;
+        float rawZPos = transform.localPosition.z + zOffset;
+        float clampedZPos = Mathf.Clamp(rawZPos, -zRange, zRange);
+
+        transform.localPosition =
+            new Vector3(
+                clampedXPos, transform.localPosition.y, clampedZPos);
     }
 }
