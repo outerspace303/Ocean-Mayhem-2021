aa0ee1c649fd32ff26b3efb8d80af9a417869140
Jakub <49901045+outerspace303@users.noreply.github.com>
2021-04-01 20:12:18 +0200

    more timeline, player control

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -8,27 +8,38 @@ public class PlayerControls : MonoBehaviour
     [SerializeField] private float controlSpeed = 10f;
     [SerializeField] private float xRange = 5f;
     [SerializeField] private float zRange = 5f;
+    [SerializeField] private float controlRollFactor = 2f;
+    [SerializeField] private float positionYawFactor = -5f;
+
+    float xThrow, zThrow;
     
     // Because of Blender: Z - right, left; Y - up, down; X - forward
+    // Rotation is affected as well.
     void Update()
     {
         ProcessTranslation();
         ProcessRotation();
     }
 
-    private void ProcessRotation()
+    void ProcessRotation()
     {
-        //transform.localRotation = Quaternion.Euler();
+        //X axis
+        float roll = zThrow * controlRollFactor;
+        float yaw = transform.localPosition.z * positionYawFactor;
+        
+        transform.localRotation = Quaternion.Euler(roll, yaw,
+            transform.localRotation.z);
+        
     }
 
     void ProcessTranslation()
     {
-        float xThrow = Input.GetAxis("Vertical");
+        xThrow = Input.GetAxis("Vertical");
         float xOffset = xThrow * Time.deltaTime * controlSpeed;
         float rawXPos = transform.localPosition.x + xOffset;
         float clampedXPos = Mathf.Clamp(rawXPos, -xRange, xRange);
 
-        float zThrow = Input.GetAxis("Horizontal");
+        zThrow = Input.GetAxis("Horizontal");
         float zOffset = zThrow * Time.deltaTime * controlSpeed;
         float rawZPos = transform.localPosition.z + zOffset;
         float clampedZPos = Mathf.Clamp(rawZPos, -zRange, zRange);
